a5c46800aab90d71e13ab0481332062768b842d2
cagataycali <cagataycali@icloud.com>
2018-02-07 07:20:55 +0300

    ✨  CLOUD

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+'use strict';var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const slugify = require('slugify');
@@ -10,31 +10,32 @@ const client = algoliasearch(functions.config().algolia.app_id, functions.config
 // Name fo the algolia index for Blog posts content.
 const ALGOLIA_INDEX_NAME = 'documents';
 
-exports.register = functions.auth.user().onCreate(event => {
-  const user = event.data; // The Firebase user.
+exports.register = functions.auth.user().onCreate((() => {var _ref = (0, _asyncToGenerator3.default)(function* (event) {
+    const user = event.data; // The Firebase user.
 
-  const { email, displayName, photoURL, uid } = user; // Details of the user.
-  const isAdmin = false;
-  const slug = slugify(displayName, {
-    replacement: '-',
-    lower: true });
+    const { email, displayName, photoURL, uid } = user; // Details of the user.
+    const isAdmin = false;
 
+    let slug = slugify(displayName, {
+      replacement: ' ',
+      lower: true }).
+    split(' ').join('');
+    // Check user slug
+    let userSnapshot = yield firestore.collection('user').where('slug', '==', slug).get();
+    if (userSnapshot.docs.length > 0) {
+      slug = `${slug}-${userSnapshot.docs.length}`;
+    }
 
-  // TODO @cagatay burada es7 compile et ve async await çalışır olsun, sonrasında slug boş mu dolu mu kontrol et ona göre -1 -2 ekle
+    return firestore.doc(`user/${uid}`).set({
+      email,
+      displayName,
+      photoURL,
+      uid,
+      isAdmin,
+      slug,
+      followers: [] });
 
-  return firestore.doc(`user/${uid}`).set({
-    email,
-    displayName,
-    photoURL,
-    uid,
-    isAdmin,
-    lastSignedInAt: new Date(),
-    createdAt: new Date(),
-    blocked: [],
-    isBanned: false,
-    slug });
-
-});
+  });return function (_x) {return _ref.apply(this, arguments);};})());
 
 exports.onDocumentUpdate = functions.firestore.document('document/{slug}').
 onUpdate(event => {
@@ -57,9 +58,11 @@ onUpdate(event => {
 
 exports.onDocumentDelete = functions.firestore.document('document/{slug}').
 onDelete(event => {
-  const document = event.data.data();
+  const document = event.data.previous.data();
 
   document.objectID = event.params.slug;
+
+  firestore.doc(`pdf/${document.slug}`).delete().catch(console.log);
 
   return client.deleteObject(document.slug, err => {
     if (err) {
